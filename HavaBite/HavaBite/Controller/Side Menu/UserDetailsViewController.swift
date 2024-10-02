a3abdae69b2c535a47e1767d28bf6ac0c8eb81e1
DanteF123 <dantemfusaro@gmail.com>
2024-10-02 18:32:28 -0400

    Created initial logic of adding friend, friends are presented in Firebase.

--- HavaBite/HavaBite/Controller/Side Menu/UserDetailsViewController.swift
+++ HavaBite/HavaBite/Controller/Side Menu/UserDetailsViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class UserDetailsViewController: UIViewController {
     var user: User?
@@ -14,6 +16,9 @@ class UserDetailsViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     
+    let db = Firestore.firestore()
+    let currentUser = Auth.auth().currentUser
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,17 +26,28 @@ class UserDetailsViewController: UIViewController {
         nameLabel.text = "\(user!.first_name) \(user!.last_name)"
 
         // Do any additional setup after loading the view.
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addFriendButtonClick(_ sender: UIButton) {
+        addFriend()
     }
-    */
+    
+}
 
+extension UserDetailsViewController{
+    
+    func addFriend(){
+        let userRef = db.collection("users").document(currentUser!.uid).collection("users").document(user!.id)
+        userRef.setData(["email":user!.email, "first_name":user!.first_name, "last_name":user!.last_name]){error in
+            if let error = error {
+                print("Error adding post: \(error)")
+            } else {
+                print("Friend added successfully")
+            }
+        }
+        
+    }
+    
 }
