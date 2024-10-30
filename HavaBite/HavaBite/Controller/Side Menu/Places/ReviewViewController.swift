5c3b2784d470ef38647260049358ac126260c81f
DanteF123 <dantemfusaro@gmail.com>
2024-10-30 19:01:48 -0400

    Creating Review screen UI

---
+++ HavaBite/HavaBite/Controller/Side Menu/Places/ReviewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewViewController.swift
+//  HavaBite
+//
+//  Created by Dante Fusaro on 10/30/24.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
